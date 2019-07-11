ff2cbd5815561ed4ae6204fc6280c2f5bf7ef577
Chen Liang <c.liang@ualberta.ca>
2019-07-11 22:45:06 +0000

    modified

--- packing-list.js
+++ packing-list.js
@@ -1,6 +1,12 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
 console.log("Kitchen stuff to pack:");
+let i = 0;
 for (let i = 0; i < packingList.length; i++) {
   console.log(packingList[i]);
+}
+
+while (i < packingList.length) {
+  console.log(packingList[i]);
+  i++;
 }
