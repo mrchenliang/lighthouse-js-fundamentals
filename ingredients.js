530b1cf3cb1f92a4cc6cbc20fb2540d0e12975f5
Chen Liang <c.liang@ualberta.ca>
2019-07-11 23:03:26 +0000

    practice while and for loops

---
+++ ingredients.js
@@ -0,0 +1,23 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+console.log("Ingredients:");
+let i = 0;
+for (let i = 0; i < ingredients.length; i++) {
+  console.log(ingredients[i]);
+}
+
+console.log("Ingredients:");
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+console.log("Ingredients:");
+for (let i = ingredients.length-1; i>0; i--) {
+  console.log(ingredients[i]);
+}
+// Write a while loop that prints out the contents of ingredients:
+
+// Write a for loop that prints out the contents of ingredients:
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
