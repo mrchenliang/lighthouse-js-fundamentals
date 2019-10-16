7f7afb824df0c9afca510b7538a338e775107f95
Chen Liang <c.liang@ualberta.ca>
2019-10-16 15:56:01 +0000

    finishing up multiplication table

--- multiplicationTable.js
+++ multiplicationTable.js
@@ -1,5 +1,15 @@
 const multiplicationTable = function(maxValue) {
+  let result = "";
+  for (var i = 0; i < maxValue + 1; i++) {
+    for (var j = 0; j < maxValue + 1; j++) {
+      if (i > 0 && j > 0) {
+        result += i * j + " ";
+      }
+    }
+    result += "\n";
+  }
 
+  return result;
 };
 
 console.log(multiplicationTable(1));
